945a6e836bef52bb4d3ad392dcdf8ab6c24dea93
Michael1935 <291140738@qq.com>
2018-03-29 22:40:33 +0800

    test

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 let index = require('./routes/index');
-
+let user = require('./routes/user');
+// 当
+app.use('/user',user);
 app.use('/',index);
 app.listen(8080);
