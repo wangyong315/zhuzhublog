03a4fd6af429b44253783e5959b6cc554e9b46b7
Michael1935 <291140738@qq.com>
2018-03-30 23:01:54 +0800

    add bootstrap

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 let express = require('express');
+let path = require('path');
 let app = express();
+// 设置模板引擎 html
+app.set('view engine', 'html');
+// 制定模板的存放的根目录
+app.set('views', path.resolve('views'));
+// 指定对于html类型的模板使用ejs方法进行渲染
+app.engine('html', require('ejs').__express);
+// 静态文件中间件
+app.user(express.static(path.resolve('node_modules')));
 let index = require('./routes/index');
 let user = require('./routes/user');
 let article = require('./routes/article');
@@ -12,6 +21,9 @@ let article = require('./routes/article');
 * /user/signout  退出
 * /articel/add 发表文章
 */
+// app.use(function(req, res, next){
+
+// });
 app.use('/article',article);
 app.use('/user',user);
 app.use('/',index);
