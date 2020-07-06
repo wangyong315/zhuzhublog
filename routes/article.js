06a4953eae77f8c7f390a9863fc74b8eefe9bf5a
wangyong315 <291140738@qq.com>
2020-07-06 22:34:39 +0800

    更新文章成功

--- routes/article.js
+++ routes/article.js
@@ -5,7 +5,7 @@ let { Article } = require('../model')
 let router = express.Router();
 
 router.get('/add', checkLogin, function(req,res){
-    res.render('article/add',{title: '发表文章'});
+    res.render('article/add',{title: '发表文章', article: {}});
 });
 
 router.post('/add', checkLogin, function(req,res){
@@ -20,7 +20,6 @@ router.post('/add', checkLogin, function(req,res){
             res.redirect('/')
         }
     })
-
 });
 
 router.get('/detail/:_id', checkLogin, function(req,res){
@@ -44,6 +43,33 @@ router.get('/delete/:_id', checkLogin, function(req,res){
         } else {
             req.flash('success', '删除文章成功')
             res.redirect('/')
+        }
+    })
+});
+
+router.get('/update/:_id', checkLogin, function(req,res){
+    const _id  = req.params._id
+    Article.findById(_id, function(err, article) {
+        if (err) {
+            req.flash('err', err)
+            res.redirect('back')
+        } else {
+            res.render('article/add',{title: '文章详情', article});
+        }
+    })
+});
+
+
+router.post('/update/:_id', checkLogin, function(req,res){
+    const _id  = req.params._id
+    let article = req.body;
+    Article.update({_id}, req.body, function (err, doc) {
+        if (err) {
+            req.flash('err', err)
+            res.redirect('back')
+        } else {
+            req.flash('success', '文章更新成功')
+            res.redirect('/article/detail/'+_id)
         }
     })
 });
